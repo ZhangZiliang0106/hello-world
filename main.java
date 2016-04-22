2a13acc6bf497a690c7efda541c4dbd837587124
ZhangZiliang0106 <ZhangZiliang0106@outlook.com>
2016-04-22 15:51:12 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("你好 世界");
+		Scanner in = new Scanner(System.in);
+		System.out.println("echo:" + in.nextLine());
+		in.close();
+		
+	}
+
+}
