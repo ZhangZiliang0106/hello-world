f1d614a89574f331c08db0fc284d09dc4c0e9196
ZhangZiliang0106 <ZhangZiliang0106@outlook.com>
2016-04-22 15:55:37 +0800

    Update main.java

--- main.java
+++ main.java
@@ -4,9 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int foot;
+		double inch;
 		Scanner in = new Scanner(System.in);
-		int price = in.nextInt();
-		System.out.println("100-" + price + "=" + (100 - price));
+		foot = in.nextInt();
+		inch = in.nextInt();
+		System.out.println("foot=" + foot + ",inch=" + inch);
+		System.out.println((int) ((foot + inch / 12) * 0.3048 * 100) + "cm");
 		in.close();
 		
 	}
