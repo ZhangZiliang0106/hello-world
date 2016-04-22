aef2d9830effa09d304901692004d17fe25c6ea1
ZhangZiliang0106 <ZhangZiliang0106@outlook.com>
2016-04-22 15:54:30 +0800

    Update main.java

--- main.java
+++ main.java
@@ -4,10 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
-		System.out.println("你好 世界");
 		Scanner in = new Scanner(System.in);
-		System.out.println("echo:" + in.nextLine());
+		int price = in.nextInt();
+		System.out.println("100-" + price + "=" + (100 - price));
 		in.close();
 		
 	}
